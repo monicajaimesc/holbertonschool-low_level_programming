e67c640cd4caedc1b57e6f017a82b3d1bc7c1880
monicajaimesc <monikmaja@gmail.com>
2019-06-20 22:28:00 +0000

    files moved

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+ */
+int main(void)
+{
+char al;
+
+for (al = 'a' ; al <= 'z' ; al++) {
+      putchar(al);
+}
+putchar('/n')
+return (0);
+}
