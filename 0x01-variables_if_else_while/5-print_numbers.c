7a0bb26de36b9fc2050b16e9975c1dc1b883afe3
monicajaimesc <monikmaja@gmail.com>
2019-06-20 22:47:34 +0000

    file 5 created

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int d;
+for (d = 0; d < 10; d++)
+{
+	printf("%d", d);
+}
+printf("\n");
+return (0);
+}
