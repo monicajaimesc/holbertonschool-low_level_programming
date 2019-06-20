d51ebd94106dae7ed7adec9c05448939cfb66e7a
monicajaimesc <monikmaja@gmail.com>
2019-06-20 22:42:28 +0000

    fixing bugs

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char d;
+for (d = 'a' ; d <= 'z' ; d++)
+{
+	putchar(d);
+}
+putchar('\n');
+return (0);
+}
