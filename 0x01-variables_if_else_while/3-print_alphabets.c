1db02f22013222a7a2f7d699c5a56bc65f03b7b6
monicajaimesc <monikmaja@gmail.com>
2019-06-24 00:01:38 +0000

    3print.c added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+   */
+int main(void)
+{
+	char al;
+	for (al = 'a' ; al <= 'z'; al++)
+	{
+		putchar(al);
+	}
+	for (al = 'A'; al <= 'Z'; al++)
+	{
+		putchar(al);
+	}
+putchar('\n');
+
+return (0);
+}
