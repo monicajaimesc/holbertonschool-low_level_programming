52613703c437559a168d6abe304193849fff15bf
monicajaimesc <monikmaja@gmail.com>
2019-06-25 19:06:12 +0000

    file 5 created

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _islower - check for alphabetic character.
+ *@n: The character to check
+ * Return: Always 0.
+ */
+int print_sign(int n)
+
+{
+	int i;
+
+	if (n > 0)
+	{
+		i = 0;
+		_putchar(43);	
+	}
+	else if (n < 0)
+	{
+		i= -1;	
+		_putchar(45);
+	}
+	else
+	{
+		i= 0;
+		_putchar(48);
+	}
+return (i);	
+}
