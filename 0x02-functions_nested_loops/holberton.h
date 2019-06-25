e5c92880fb991589d2a06f57cae5393fca104b53
monicajaimesc <monikmaja@gmail.com>
2019-06-25 23:16:19 +0000

    holberton.h uploaded again

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -6,5 +6,11 @@ int _putchar(char c);
 void print_alphabet(void);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void)
+int add(int, int);
 
 #endif
