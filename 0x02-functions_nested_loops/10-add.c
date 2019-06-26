6a0d85d037b23ceaee05d23addd213c19e2f9138
monicajaimesc <monikmaja@gmail.com>
2019-06-26 02:22:54 +0000

    file 10 created

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* add - Entry point
+*@a: integer 1
+*@b: integer 2
+* Return: Always 0 (Success)
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+return (c);
+}
