23e545b12fa54e8f154acd77cca729cc2d5d08c1
monicajaimesc <monikmaja@gmail.com>
2019-06-28 02:04:16 +0000

    file 4 and 3 created

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - prints the numbers from 0 to 9, followed by a new line
+ *
+ *
+ * Return: number
+ */
+
+void print_numbers(void)
+{
+
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
