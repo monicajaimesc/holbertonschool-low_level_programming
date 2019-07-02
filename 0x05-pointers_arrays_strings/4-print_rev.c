ce3d74adb7d2c8a520cdc543d5b94eef0560d270
monicajaimesc <monikmaja@gmail.com>
2019-07-02 18:32:17 +0000

    file 4 created

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+* print_rev - prints a string in reverse
+*
+*@s: pointer variable
+*return: nothing
+*/
+
+void print_rev(char *s)
+{
+	int phrarev, cont;
+
+	cont = _strlen(s);
+
+	for (phrarev = cont; phrarev >= 0; phrarev--)
+	{
+		_putchar(s[phrarev]);
+	}
+	_putchar('\n');
+}
+
+/**
+* _strlen - returns the length of a string
+*@s: pointer variable
+* Return: nothing
+*/
+int _strlen(char *s)
+{
+	int cont = 0;
+
+	while (s[cont])
+
+	{
+		cont++;
+	}
+	return (cont);
+}
