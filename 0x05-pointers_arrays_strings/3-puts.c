9a2e847382a216a1326a94b3d22fb6977e8fb172
monicajaimesc <monikmaja@gmail.com>
2019-07-02 15:37:12 +0000

    file 3 created

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _puts - function that prints a string
+*@str: pointer variable
+* Return: nothing
+*/
+
+void _puts(char *str)
+{
+	int printstr;
+
+	while (str[printstr])
+
+	{
+		_putchar(str[printstr]);
+		printstr++;
+	}
+_putchar('\n');
+}
