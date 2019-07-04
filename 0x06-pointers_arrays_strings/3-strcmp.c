065c39b9fdaa6af9ab1b307f61cfc954362a0020
monicajaimesc <monikmaja@gmail.com>
2019-07-04 22:12:39 +0000

    file 3 added

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+/**
+ * _strcmp - compares two strings
+ *@s1 : destino pointer variable
+ *@s2 : source
+ *
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
