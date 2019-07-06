9325219e86ef006a6ed144c5c78ffa01d8cf1c71
monicajaimesc <monikmaja@gmail.com>
2019-07-06 01:42:02 +0000

    file 8 created

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -12,12 +12,27 @@ char *rot13(char *s)
 {
 	int i, j;
 
-	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char o[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZ";
+	char a[52] = {'a',
+		'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm', 'n', 'o',
+		'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z', 'A', 'B', 'C',
+		'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+		'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z'};
+	char o[52] = {'n', 'o', 'p', 'q', 'r',
+		's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'a', 'b', 'c', 'd', 'e', 'f',
+		'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+		'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B',
+		'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j <= 52; j++)
+		for (j = 0; j <= 51; j++)
 		{
 
 			if (s[i] == a[j])
