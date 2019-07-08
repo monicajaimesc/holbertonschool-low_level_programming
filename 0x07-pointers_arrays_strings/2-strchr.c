8e711f18b3863a1fd962754210a0bfb49e5801f1
monicajaimesc <monikmaja@gmail.com>
2019-07-08 18:52:55 +0000

    file 2 added

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchr - locates a character in a string
+ *@s : string
+ *@c : first ocurrence of the character
+ *
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+return (0);
+}
