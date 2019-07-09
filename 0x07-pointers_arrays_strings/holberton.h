63d105b4c34b548b7f6685c48fc790db8f184070
monicajaimesc <monikmaja@gmail.com>
2019-07-09 03:45:48 +0000

    holberton missed

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -8,5 +8,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
 
 #endif
