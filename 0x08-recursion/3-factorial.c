8ef6a63da77b5718a3e4f47408a9f40e10bb410c
monicajaimesc <monikmaja@gmail.com>
2019-07-10 21:00:07 +0000

    file 3 created

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * factorial - factorial of a given number
+ *@n : number
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+return (n * factorial(n - 1));
+}
