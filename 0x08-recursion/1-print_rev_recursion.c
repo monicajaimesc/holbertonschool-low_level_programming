850dd9e40e43fa80bd254f133f9e6711505740d0
monicajaimesc <monikmaja@gmail.com>
2019-07-10 18:32:26 +0000

    file 1 added

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,12 @@
  */
 void _print_rev_recursion(char *s)
 {
+	char ss = *s;
+
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	_print_rev_recursion(++s);
+	_putchar(ss);
 }
