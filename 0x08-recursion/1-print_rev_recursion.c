e6370fc3c79959d5520ebc726b7f41482372279c
monicajaimesc <monikmaja@gmail.com>
2019-07-10 17:44:05 +0000

    file 1 added

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ *@s : string
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
