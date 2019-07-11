c433f4e0cc20932d69ed0e5d76ef12148bb91365
monicajaimesc <monikmaja@gmail.com>
2019-07-11 04:20:54 +0000

    file 4 modificated with a new version

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,13 +15,13 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	}
 
-	if (y != 0)
-	{ 
-		return(x * _pow_recursion(x, y - 1));
+	if (y == 0)
+	{
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (x * _pow_recursion(x, y - 1));
 
 	}
 }
