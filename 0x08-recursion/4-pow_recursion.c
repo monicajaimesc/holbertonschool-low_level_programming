8b5a3e3ac2c3502d8f070dd46be28a3dd22a4139
monicajaimesc <monikmaja@gmail.com>
2019-07-11 04:10:56 +0000

    adding file 4 doesnt want to compile it

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - x raised to the power of y
+ *@x : value
+ *@y : power (raised to)
+ * Return: x
+ */
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y != 0)
+	{ 
+		return = (x * _pow_recursion(x, y - 1);
+	}
+	else
+	{
+		return (1);
+
+	}
+}
