59a798a01afa7ba80ef25ef89c2159b23d13c530
monicajaimesc <monikmaja@gmail.com>
2019-07-14 18:46:22 +0000

    files added to the correcto folder

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - program name, followed by a new line
+ *
+ *@argc: argument count
+ *@argv: argument value
+ * Return: zero
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
