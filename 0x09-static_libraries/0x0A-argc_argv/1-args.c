0411842699efc9639d60b53b56fb8b28b11b5353
monicajaimesc <monikmaja@gmail.com>
2019-07-14 18:25:33 +0000

    1 args.c file added

---
+++ 0x09-static_libraries/0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - prints the number of arguments
+ *
+ *@argc: argument count
+ *@argv: argument value vector
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+
+	printf("%d\n", argc - 1);
+
+return (0);
+}
+
+
