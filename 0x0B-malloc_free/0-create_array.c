f9b0fa3284879f002df2cc7d42fbb634012b5720
monicajaimesc <monikmaja@gmail.com>
2019-07-16 19:00:53 +0000

    first files added with the 0 task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_array - creates an array of chard
+ *
+ *@size: size unsigned int type
+ *@c: constant char type
+ * Return: 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *b = (char *) malloc(size * sizeof(char));
+
+	if (size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		*(b + i) = c;
+	}
+
+return (b);
+}
