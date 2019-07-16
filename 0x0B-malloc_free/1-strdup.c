f8eab74e4bd83c7ed0ac1f921c713e6604706f97
monicajaimesc <monikmaja@gmail.com>
2019-07-16 21:10:57 +0000

    file 1 created

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+* _strlen - returns the length of a string
+*@s: pointer variable
+* Return: nothing
+*/
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count])
+		count++;
+	{
+		return (count);
+	}
+}
+
+
+/**
+ * _strdup - return a pointer to a newly space in memory
+ *
+ *@str: pointer that saved the first char's direction
+ *
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	unsigned int size;
+	char *ss;
+	unsigned int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = _strlen(str);
+
+	ss = (char *) malloc(size * sizeof(char));
+
+	if (ss == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		*(ss + i) = *(str + i);
+	}
+
+return (ss);
+}
