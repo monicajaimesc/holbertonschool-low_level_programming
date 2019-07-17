c34c7601b3f84731b8590f14676fdb347942091a
monicajaimesc <monikmaja@gmail.com>
2019-07-17 05:22:55 +0000

    file 100 created

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words
+ * @strtow : ponter
+ * @str : value
+ *
+ * Return: string
+ */
+char **strtow(char *str)
