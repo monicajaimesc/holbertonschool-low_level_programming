0ad98083d520935b80fe956fa5661fc6f4d035c0
monicajaimesc <monikmaja@gmail.com>
2019-07-17 05:17:59 +0000

    file 5 created

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * argstostr - Concatenates all the arguments of your program.
+ *
+ * @ac: count
+ * @av: values
+ *
+ * Return: pointer should point to a new string.
+ */
+char *argstostr(int ac, char **av)
+{
