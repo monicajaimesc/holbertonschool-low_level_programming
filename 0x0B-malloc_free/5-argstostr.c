c3d8bbf0f2f5406f515b138c1916aa6fe3f33aea
monicajaimesc <monikmaja@gmail.com>
2019-07-18 02:45:59 +0000

    file 5 compiled

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -3,6 +3,22 @@
 #include <stdio.h>
 
 /**
+* _strlen - returns the length of a string
+*@s: pointer variable
+* Return: nothing
+*/
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+		count++;
+	{
+		return (count);
+	}
+}
+
+/**
  * argstostr - Concatenates all the arguments of your program.
  *
  * @ac: count
@@ -12,3 +28,27 @@
  */
 char *argstostr(int ac, char **av)
 {
+	char *p;
+	int i, j, sum = 0, count = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		sum += _strlen(*(av + i));
+	p = malloc(sizeof(char) * (sum + ac + 1));
+	if (p == NULL)
+		return (NULL);
+	for (j = 0; j < ac; j++)
+	{
+		for (i = 0; i < _strlen(*(av + j)); i++)
+		{
+			p[count] = *(*(av + j) + i);
+			count++;
+		}
+		p[count] = '\n';
+		count++;
+	}
+	p[count] = '\0';
+	return (p);
+}
+
