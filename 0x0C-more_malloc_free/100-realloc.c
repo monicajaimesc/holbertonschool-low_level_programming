ce0a12f49f346dcfb1aa4ec246f5d60974dadf77
monicajaimesc <monikmaja@gmail.com>
2019-07-19 03:14:20 +0000

    file 100 was created

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+*_realloc - reallocates a memory block 
+*@old_size : old size
+*@new_size : new size
+* Return: pointer
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+
+
+}
