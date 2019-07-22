0fb239a5bad2b8f78b3288418f1cc31e2b548455
monicajaimesc <monikmaja@gmail.com>
2019-07-22 21:19:16 +0000

    dog.h and holberton.h added

---
+++ 0x0D-preprocessor/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
