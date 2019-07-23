430e4b63502b10c45c60974fab809bdc00d30f44
monicajaimesc <monikmaja@gmail.com>
2019-07-23 13:26:39 +0000

    file 2 modificated with comments

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file
+ * main - prints the name of the file it was compiled from
  *
  * Return: Always 0.
  */
@@ -10,3 +10,6 @@ int main(void)
 	printf("%s\n", __FILE__);
 	return (0);
 }
+/*__FILE__ standard predefined macro*/
+/*this macro expands to the name of the current input file*/ 4/*in the form of a C string constant*/
+
