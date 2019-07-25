99b9ec078f6cb48e4972cce041d67bf476305592
monicajaimesc <monikmaja@gmail.com>
2019-07-25 19:30:20 +0000

    comments writed

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,13 +9,14 @@
  *as a parameter
  */
 int (*get_op_func(char *s))(int, int)
-{
+{ /*array of structure of op_t and it has 2 members*/
+ /*char *op and int *f (int, int)*/
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
+	/*0*/	{"+", op_add}, /*+ is an string */
+	/*1*/	{"-", op_sub},
+	/*2*/	{"*", op_mul},
+	/*3*/	{"/", op_div},
+	/*4*/	{"%", op_mod},
 		{NULL, NULL}
 	};
 	int i = 0;
@@ -28,5 +29,5 @@ int (*get_op_func(char *s))(int, int)
 	}
 	printf("Error\n");
 	exit(99);
-}
-
+}i
+/*function that will return a pointer a una funcion que recibe dos parametros y esa funcion me retorna un int*/
