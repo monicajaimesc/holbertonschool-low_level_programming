329f6b72f98a52e5df17e208c538a538765368a8
monicajaimesc <monikmaja@gmail.com>
2019-07-27 18:24:13 +0000

    function that prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,14 +14,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numlist;
 	unsigned int i;
 
-	if (separator == NULL)
-		return;
 	va_start(numlist, n); /*Initialize argument list*/
 	for (i = 0; i < n; i++)
 	{
-		printf("%d%s", va_arg(numlist, int), separator);
+		printf("%d", va_arg(numlist, int));
+		if (separator == NULL)
+			continue;
+		else if (i == (n - 1))
+			continue;
+		else
+			printf("%s", separator);
 	}
-
 	printf("\n");
 	/*va_arg to get data connect to numlist*/
 	va_end(numlist); /*clean up */
