5e8d71d1b61d512442d7e7bd7054fa31c03bb0b8
monicajaimesc <monikmaja@gmail.com>
2019-07-27 18:04:30 +0000

    3 printall

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,57 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - print anything
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ * Return: nothing, void
+ */
+void print_all(const char *const format, ...)
+{
+	char *str;
+	unsigned int i = 0;
+	va_list all;
+
+	va_start(all, format);
+
+		while (format[i] == '\0')
+		{
+			printf("\n");
+			return;
+		}
+		while (format[i] != '\0')
+		{
+			switch (format[i])
+		{
+		case 'c':
+			printf("%c", (char)va_arg(all, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(all, int));
+			break;
+		case 'f':
+			printf("%f", (float)va_arg(all, double));
+			break;
+		case 's':
+			str = va_arg(all, char *);
+			if (str != NULL)
+			{
+				printf("%s",str);
+				break;
+			}
+			printf("(nil)");
+			break;
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 's' ||
+			format[i] == 'f') && (format[i + 1] != '\0'))
+			printf(", ");
+		i++;
+
+		}
+		printf("\n");
+		va_end(all);
+
+}
