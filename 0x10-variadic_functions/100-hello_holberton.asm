11246243468d2a9549c57fd6fcf5d203e6a93c5e
monicajaimesc <monikmaja@gmail.com>
2019-08-04 20:18:13 +0000

    program in assembly that prints hello, holberton

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,14 @@
+section .data
+	text db "Hello, Holberton", 10
+section .text
+	global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text
+	mov rdx, 17
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
