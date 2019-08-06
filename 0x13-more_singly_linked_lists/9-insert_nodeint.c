d895c9b8f057e744445f46b81a436d62b059341e
monicajaimesc <monikmaja@gmail.com>
2019-08-06 16:20:55 +0000

    fixing bugs

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,6 +3,26 @@
 #include "lists.h"
 
 /**
+ * listint_len - returns the number of elements in a linked list_t list
+ *@h: header pointer
+ *
+ * Return: number of elements in the linked list_t list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t counter = 0;
+
+	while (h)
+	{
+		h = h->next; /*now the header is the nex value of the list*/
+		counter++;
+	}
+
+return (counter);
+}
+
+/**
  * insert_nodeint_at_index - inserts a new node at a given position
  *@head: pointer to the head
  *@idx: index of the list where the new node should be added
@@ -21,8 +41,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-
 	temp = *head;
+
+	
 	while (temp)
 	{
 		if (counter + 1 == idx)
@@ -34,6 +55,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		temp = temp->next;
 		counter++;
+	}
+	
+	if (idx > listint_len(temp))
+	{
+	
+		return (NULL);
 	}
 return (*head);
 }
