cae2c1d49710b77161ca0e51cff1f82f27ed6c29
monicajaimesc <monikmaja@gmail.com>
2019-08-06 20:08:54 +0000

    while go thorug temporal

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	temp = *head;
 
-	while (head)
+	while (temp)
 	{
 		/*find previos node of node to be deleted*/
 		if ((counter + 1) == index)
