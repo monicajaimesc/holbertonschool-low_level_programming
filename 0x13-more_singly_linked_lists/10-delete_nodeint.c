45fa4f2a52f9f69e769a904357ba769406014518
monicajaimesc <monikmaja@gmail.com>
2019-08-06 19:54:03 +0000

    fixing one bug

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,14 +13,15 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp;
 	listint_t *nodo;
 	unsigned int counter = 0;
-	
+
 	if (*head == NULL) /*head value is null return -1*/
 		return (-1);
 	temp = *head;
 	if (index == 0)
 	{
+		nodo = temp;
 		*head = temp->next; /*change head*/
-		free(temp);/*free old head*/
+		free(nodo);/*free old head*/
 		return (1);
 	}
 
@@ -32,10 +33,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			nodo = temp->next;/*nodowill save the value tempnext*/
 			temp->next = (temp->next)->next;
 			free(nodo);
-			break;
+			return (1);
 		}
 	temp = temp->next;
 	counter++;
 	}
-return (1);
+return (-1);
 }
