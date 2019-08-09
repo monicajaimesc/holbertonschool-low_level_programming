a7814d4088f69125368be3c9e47cf1c90f7852e7
monicajaimesc <monikmaja@gmail.com>
2019-08-09 00:04:07 +0000

    print binary representation in a easy code :)

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,42 +7,15 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int index, counter = 0;
-
-	if (n == 0)
+/*third, least than 2 is 0 and 1, print it and return*/
+	if (n < 2)
 	{
-		_putchar ('0');/*0 binary evaluation*/
+		_putchar(n + '0');
 		return;
 	}
 
-	if (n == 1)
-	{
-		_putchar('1');/*1 binary evaluation*/
-		return;
-	}
-	/*convertion to binary number*/
-	index = 2; /*always is 2*/
-
-	while (index <= n && counter < 62) /* 62 because we're working in machine 64*/
-	{
-		index <<= 1; /* move one to left*/
-		counter++; /*save the result*/
-	}
-	if (counter != 62)
-		index >>= 1; /*move one position to the right*/
-
-	while (index != 0)
-	{
-		if ((index & n) != 0)/*do the & evaluation, 1 1 = 1*/
-		{
-			_putchar('1');
-			counter++;
-		}
-		else
-		{
-			_putchar('0'); /*do the 0 evaluation*/
-			counter++;
-		}
-		index >>= 1;
-	}
+	print_binary(n >> 0x01); /*primero, recursion, move to the right*/
+/*second,do the division (see example) & convert the binary to a number + '0'*/
+	_putchar((n & 0x01) + '0');
+	/*example, var=3; >>4 -> 3/2 a la 4*/
 }
