6913130707580164935ead566bcef9982080726c
monicajaimesc <monikmaja@gmail.com>
2019-08-15 02:25:31 +0000

    header fixed

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,7 +1,11 @@
 #ifndef HOLBERTON_H
 #define	HOLBERTON_H
-
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
