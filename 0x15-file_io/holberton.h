0ed79c32c82ddb37130d22bd2b4f8ff533a98aa9
monicajaimesc <monikmaja@gmail.com>
2019-08-17 03:13:27 +0000

    header

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -5,6 +5,10 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
