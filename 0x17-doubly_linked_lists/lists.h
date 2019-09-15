82ac0195bb151dda1cfef7eb35b58c531ca893bd
monicajaimesc <monikmaja@gmail.com>
2019-09-15 23:52:25 +0000

    header actualized

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -23,7 +24,7 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 
 #endif /* LIST_H */
