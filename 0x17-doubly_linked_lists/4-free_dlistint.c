a7d4d20e1bb344bfb1f0aa2aa586f91ce94bce21
monicajaimesc <monikmaja@gmail.com>
2019-09-16 02:02:55 +0000

    function that free a list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_dlistint - function that free a list
+ *@head: head pointer
+ *
+ *Return: no return, is a void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
+
+		
