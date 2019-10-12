d11aaa6a2abbfb9d9586d7fbf063eb457df3e4ee
monicajaimesc <monikmaja@gmail.com>
2019-10-12 00:17:52 +0000

    library

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -9,6 +9,7 @@
 
 
 
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -45,7 +46,7 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 
 
