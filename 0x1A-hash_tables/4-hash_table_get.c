6737ccc45f11e019e27fea8189b8433efddb0f84
monicajaimesc <monikmaja@gmail.com>
2019-10-12 04:29:25 +0000

    function and library added

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -2,27 +2,33 @@
 /**
  * hash_table_get - retrieves a value associated with a key
  *@ht: hash table you want to look into
- *@key: key you are looking for 
+ *@key: key you are looking for
  * Return: the value associated element orNULL if key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned int index;
-    hash_node_t *node;
+	unsigned int index;
+	hash_node_t *node;
 
-    if( !key || !ht || !ht->array)
+	if (!key || !ht || !ht->array)
 		return (0);
-    index = key_index((unsigned char *)key, ht->size);
-    node = ht->array[index];
-    while (node != NULL)
-    {
-        if (strcmp(node->key, key) == 0)
-        {
-            return (node->value);
-        }
-        node = node->next;
+	/* return key string "c" */
+	index = key_index((unsigned char *)key, ht->size);
+	/* node is point to the table in index position */
+	node = ht->array[index];
+	/* will go throught to the end linked list */
+	while (node != NULL)
+	{
+		/* compare value and if it's the same return value */
+		if (strcmp(node->key, key) == 0)
+		{
+			/* return value of the key */
+			return (node->value);
+		}
+	/* aumente nodo */
+	node = node->next;
 
-    }
-    return (NULL);
+	}
+	return (NULL);
 }
 
