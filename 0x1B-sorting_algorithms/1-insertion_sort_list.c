4f9466853aa918162fab06c220a31a2a2174189f
monicajaimesc <monikmaja@gmail.com>
2019-10-30 00:21:14 +0000

    finally done

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,26 +1,44 @@
 #include "sort.h"
 /**
- *insertion_sort_list - sorts a doubly linked list of integers in ascending order using the Insertion sort 
+ *insertion_sort_list - sorts a doubly linked list
+ *in ascending order using the Insertion sort
  *@list: list to be sort
  *Return: nothing is a void
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *actual_node, *next_node;
+	listint_t *p, *first;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	actual_node = *list;
-	while (actual_node != NULL)
+	p = *list;
+	while (p != NULL)
 	{
-		next_node = actual_node->next;
-        
+		/* go throught */
+		first = p->next;
+		/* compare mayor(99) > menor(71), sorting list */
+		while (p->prev != NULL && p->prev->n > p->n)
+		{
+			/* display the list */
+			p->prev->next = p->next;
+			if (p->next != NULL)
+				/* lo devuelvo */
+				p->next->prev = p->prev;
 
-
-
-
-
-
-
+			p->next = p->prev;
+			p->prev = p->next->prev;
+			p->next->prev = p;
+			if (p->prev == NULL)
+			{
+				*list = p;
+			}
+			else
+				p->prev->next = p;
+			print_list(*list);
+		}
+		/* list ended where it started */
+		p = first;
+	}
+}
