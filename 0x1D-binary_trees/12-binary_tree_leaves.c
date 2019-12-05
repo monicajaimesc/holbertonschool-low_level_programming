1d62fefc0e5331d38a616621dc2a21da06de1158
monicajaimesc <monikmaja@gmail.com>
2019-12-05 21:31:48 +0000

    binary tree leaces

--- 0x1D-binary_trees/12-binary_tree_leaves.c
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -11,6 +11,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	size_t size_left;
 	size_t size_right;
 
+	if (tree == NULL)
+		return (0);
+
 	if (tree->left != NULL)
 		size_left = (binary_tree_leaves(tree->left));
 	else
