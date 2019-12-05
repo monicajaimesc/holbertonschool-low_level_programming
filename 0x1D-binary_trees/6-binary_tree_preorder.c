a566aeb1f6700d5f34cae4f49fb0fd31497be7fb
monicajaimesc <monikmaja@gmail.com>
2019-12-05 21:17:23 +0000

    binary tree preoirder fixed

--- 0x1D-binary_trees/6-binary_tree_preorder.c
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -11,6 +11,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 
 	if (tree == NULL)
 		return;
+	if (func == NULL)
+		return;
 
 	function_call(tree->n);
 	if (tree->left != NULL)
